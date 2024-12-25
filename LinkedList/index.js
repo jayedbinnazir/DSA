939445cd113e76161572798e23aeffe44ef051a2
jayedbinnazir <jayed.freelance@gmail.com>
2024-12-25 17:08:14 +0600

    Linked List Implemented

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -1,1 +1,117 @@
-console.log("Jayed");
+console.log("Linked List");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  appened(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
+  }
+
+  inSert(value, index) {
+    if (index < 0 || index > this.size) {
+      console.log("index is greater than size");
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+    }
+  }
+
+  removeFrom(index) {
+    if (index < 0 || index > this.size) {
+      console.log("Invalid Index");
+      return null;
+    }
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is Empty");
+      return;
+    } else {
+      let curr = this.head;
+      let listValue = "";
+      while (curr) {
+        listValue += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValue);
+    }
+  }
+}
+
+const list = new LinkedList();
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.appened(60);
+list.inSert(33, 2);
+list.print();
+console.log("After removing element by index");
+list.removeFrom(2);
+list.print();
+
+console.log("list is empty", list.isEmpty());
+console.log("list size", list.getSize());
+list.print();
